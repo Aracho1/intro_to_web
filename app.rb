f1d0d16ede4e0e5567c960b9c9ba1139dcd5655a
Sujeevan Rajagopalan <rsujee@MA307.local>
2020-12-07 17:42:49 +0000

    Introducing params

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ get '/' do
   "hello!"
 end
 
-get '/cat'do
+get '/random-cat' do
  @random_name = ["Amigo", "Oscar", "Viking"].sample
  erb :index
 end
+
+get '/named-cat' do
+  p(params)
+  @random_name = params[:name]
+  erb :index
+ end
